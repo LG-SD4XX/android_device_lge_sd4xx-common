b72d4be0072423c9ce54917c4d78273bbca3a500
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    msm8937-common: Build Display HIDL HAL interfaces
     * Enable binderized Gralloc HAL along with it
     * Enable HWC2
    
    Change-Id: I61c34a531e3131209a08b5b4670e1f1f8fbb435d

--- sd4xx.mk
+++ sd4xx.mk
@@ -105,6 +105,14 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
     $(COMMON_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml
 
+# Display HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
