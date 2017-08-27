a9d05e979bdfd482103f151d450f3606047c2d8e
Luca Stefani <luca020400@lineageos.org>
2017-08-27 20:32:05 +0200

    msm8937-common: Remove references to hostapd
    * No longer used.
    * It lives in it's own rc extension now
    
    Change-Id: I0c53cbb1ba2610aac4349b6a0e66e23c75871d27
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -19,15 +19,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
-
-ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := hostapd_default.conf
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
-LOCAL_SRC_FILES    := hostapd.conf
-include $(BUILD_PREBUILT)
-
-endif
