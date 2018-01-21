2c59635a88cbfe07fa3d5c78c4ea9143726d1b3d
Prateek Chaubey <chaubeyprateek@gmail.com>
2018-01-21 08:19:03 +0000

    msm8937-common: Build Configstore HIDL HAL interfaces

--- sd4xx.mk
+++ sd4xx.mk
@@ -93,6 +93,10 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service
+
 # HIDL Packages
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
