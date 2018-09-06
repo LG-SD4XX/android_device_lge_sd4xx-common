b66db7694d69508b9bd5fb97bfc71dbd1cfd2686
travisholt92 <Travisholt92@gmail.com>
2018-09-05 23:48:37 -0400

    remove uevent.qcom.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -89,11 +89,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
