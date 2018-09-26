ee80897b51ce410b33d80c6eba0ae7d5311a8b0d
deadman96385 <seanhoyt963@gmail.com>
2018-09-25 21:21:29 -0500

    List additional native libraries to expose to apps

--- sd4xx.mk
+++ sd4xx.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/public.libraries.txt:system/etc/public.libraries.txt
+
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
