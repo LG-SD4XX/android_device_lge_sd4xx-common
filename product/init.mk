c90c24b506357ff512d02c816aa0eb22419b969a
travisholt92 <Travisholt92@gmail.com>
2018-10-01 20:15:15 -0400

    fix more merge derps

--- product/init.mk
+++
@@ -1,7 +0,0 @@
-# Init scripts
-PRODUCT_PACKAGES += \
-    fstab.qcom \
-    init.qcom.rc \
-    init.qcom.sh \
-    init.target.rc \
-    ueventd.qcom.rc
--- product/qcom-audio.mk
+++
@@ -1,6 +0,0 @@
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=fluence \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false
